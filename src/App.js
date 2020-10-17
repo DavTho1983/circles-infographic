a3c061548b9bd0cec2d7c62188667bdd6b96f421
DavTho1983 <david.alex.thompson@gmail.com>
2020-10-17 12:09:44 +0100

    corrects for large values

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "./styles.css";
 
 export default function App() {
@@ -6,17 +6,12 @@ export default function App() {
   //circles[1] = r2
   //circles[2] = r3
 
-  const circles = [60, 60, 40];
+  let circles = [40, 40, 40];
+  circles = circles.sort()
   const colours = ["green", "blue", "red"];
 
-  console.log("LENGTH", 2 * Math.sqrt(circles[0] * circles[1]))
-
   let circle2xPos =
-    2 * Math.sqrt(circles[0] * circles[1]) + circles[0]; // What Dr E has
-
-  let r1r2 = circles[0] + circles[1];
-  let r1r3 = circles[0] + circles[2];
-  let r2r3 = circles[1] + circles[2];
+    2 * Math.sqrt(circles[0] * circles[1]); // What Dr E has
 
   let theta1 = Math.asin((circles[0] - circles[1]) / (circles[0] + circles[1]))
   let theta2 = Math.acos(((circles[1] + circles[2]) + (circles[0] + circles[1]) + (circles[0] + circles[2])) / (2 * (circles[1] + circles[2]) * (circles[0] + circles[1])))
@@ -26,11 +21,8 @@ export default function App() {
   console.log("ZPETA", zeta, peta)
   let H = Math.sin(zeta) * (circles[1] + circles[2])
   console.log("H", H)
-  let X = circles[0] -circles[2] + (2 * Math.sqrt(circles[0] * circles[1])) - Math.sin(peta) * H
+  let X = -circles[2] + (2 * Math.sqrt(circles[0] * circles[1])) - Math.sin(peta) * H
   console.log("X", X)
-
-  // let ycoord = Math.cos(newAngle) * r1r3
-  // let xcoord = Math.sin(newAngle) * r1r2
 
   const getCircleY = index => {
     let yPos = 0;
@@ -46,9 +38,9 @@ export default function App() {
 
     console.log(circles);
     for (let i = 0; i < index + 1; i++) {
-      console.log(circles[i]);
+      console.log(circles[0]);
       if (i === 0) {
-        xPos = circles[1];
+        xPos = 0;
       } else if (i === 1) {
         xPos = circle2xPos;
         console.log("xPos", xPos);
@@ -61,7 +53,7 @@ export default function App() {
   };
 
   const getCircles = () => {
-    return circles.map((item, index) => (
+    let _circles = circles.reverse().map((item, index) => (
       <div
         className="circle"
         style={{
@@ -73,14 +65,47 @@ export default function App() {
           padding: 0,
           margin: 0,
           backgroundColor: colours[index],
-          borderRadius: "50%"
+          borderRadius: "50%",
         }}
       />
     ));
+    let _shiftx = 0
+    for (let circle of _circles) {
+      console.log(circle.props.style)
+      if (circle.props.style.left < 0) {
+        console.log("SHIFT RIGHT", 1 - circle.props.style.left)
+        _shiftx = 1 - circle.props.style.left
+      }
+      if (circle.props.style.top > (circles[0] + circles[1])) {
+        circle.props.style.top = (circles[0])
+      }
+    }
+    if (_shiftx !== 0) {
+      for (let circle of _circles) {
+        circle.props.style.left = circle.props.style.left + _shiftx
+      }
+    }
+    console.log("NEW CIRCLES", _circles)
+    return _circles
   };
   return (
     <div className="App">
-      <div className="circles">{getCircles()}</div>
+      <div className="circles" ref={el => {
+        // el can be null - see https://reactjs.org/docs/refs-and-the-dom.html#caveats-with-callback-refs
+        if (!el) return;
+
+        console.log("Bounding RECT", el.getBoundingClientRect().width); // prints 200px
+        
+      }}
+      style={{
+        position: "absolute",
+        top: 0,
+        left: 0,
+        height: 300,
+        width: 300,
+        padding: 0,
+        margin: 0
+      }}>{getCircles()}</div>
     </div>
   );
 }
